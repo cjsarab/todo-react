6f4913e1de72cf19c4a10adce1bef732be8e5bcc
Cammy Smith <cjsarab1991@gmail.com>
2022-10-10 16:00:46 +0100

    Added priority

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import React, {useState} from 'react';
 function App() {
 
   const [tasks, setTasks] = useState([
-    {name: "Wash Dishes", isDone: false},
-    {name: "Get Petrol", isDone: false},
-    {name: "Paint Ceiling", isDone: false},
-    {name: "Remove Bugs", isDone: false},
+    {name: "Wash Dishes", isDone: false, priority: true},
+    {name: "Get Petrol", isDone: false, priority: false},
+    {name: "Paint Ceiling", isDone: false, priority: true},
+    {name: "Remove Bugs", isDone: false, priority: true},
   ]);
 
   const [newTask, setNewTask] = useState("");
 
   const taskNodes = tasks.map((task, index) => {
     return(
+      <>
       <li key={index} className={task.isDone ? "done" : "not-done"}>
         <span> {task.name} </span>
         {task.isDone ? <span className="done">Done!</span> :
         <button onClick={() => completeTask(index)}>Complete?</button>}
       </li>
+
+      <li key={index} className={task.priority ? "high" : "low"}>
+        <span> {task.priority} </span>
+        {task.priority ? <span className="high"> High priority! </span> :
+        <button onClick ={() => changePriority(index)}>Make high priority?</button>}
+      </li>
+
+      <li key={index} className="delete-button">
+        <button onClick={() => deleteTask(index)}>Delete?</button>
+      </li>
+      </>
     );
   });
 
@@ -29,7 +41,7 @@ function App() {
   const saveNewTask = (e) => {
     e.preventDefault();
     const copyTasks = [...tasks];
-    copyTasks.push({name: newTask, isDone: false});
+    copyTasks.push({name: newTask, isDone: false, priority:false});
     setTasks(copyTasks);
     setNewTask("");
   };
@@ -39,6 +51,18 @@ function App() {
     copyTasks[index].isDone = true;
     setTasks(copyTasks);
   };
+
+  const deleteTask = (index) => {
+    const copyTasks = [...tasks];
+    copyTasks[index].remove();
+    setTasks(copyTasks);
+  }
+
+  const changePriority = (index) => {
+    const copyTasks = [...tasks];
+    copyTasks[index].priority = true;
+    setTasks(copyTasks);
+  }
 
   return (
     <>
